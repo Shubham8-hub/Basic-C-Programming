a66457850f298c60dc00114c761dbe9258178fcd
Shubham Samal <shubham.saamal@gmail.com>
2022-10-27 22:58:32 +0530

    27-10-2022

---
+++ Asteric.c
@@ -0,0 +1,19 @@
+// To print astrtic in rectangle shape
+/*
+                **********    
+                **********    
+                **********    
+                **********    
+                **********    
+*/
+
+#include "stdio.h"
+
+int main(){
+    printf("**********\n");
+    printf("**********\n");
+    printf("**********\n");
+    printf("**********\n");
+    printf("**********\n");
+    return 0;
+}
